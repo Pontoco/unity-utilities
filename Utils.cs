4bcdc5ff537ef1d3807be5f70c1c5a03022b7f73
John Austin <kleptine@gmail.com>
2017-04-18 11:10:18 -0700

    A few enum utilities and an extra util method to Unwrap an Optional.

---
+++ Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Optional;
+using UnityEngine;
+
+public static class Utils {
+  public static class EnumUtil {
+    public static IEnumerable<T> GetEnumValues<T>() {
+      return Enum.GetValues(typeof(T)).Cast<T>();
+    }
+  }
+
+  public static T UnwrapOrDefault<T, V>(this Option<V> option, Func<V,T> map, T default_value) {
+    return option.Match(some: map, none: () => default_value);
+  }
+}
