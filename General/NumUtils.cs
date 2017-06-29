1d861b737a73dbda789c06c60df993a75b98291e
John Austin <kleptine@gmail.com>
2017-06-29 11:44:26 -0700

    Better organize Unity utils into separate directory.

--- General/NumUtils.cs
+++ General/NumUtils.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using UnityEngine;
 
 namespace Utilities {
   public class NumUtils {
-    /// <summary>
-    ///   Determine the signed angle between two vectors, with normal 'n'
-    ///   as the rotation axis.
-    /// </summary>
-    public static float AngleSigned(Vector3 v1, Vector3 v2, Vector3 n) {
-      return Mathf.Atan2(Vector3.Dot(n, Vector3.Cross(v1, v2)), Vector3.Dot(v1, v2)) * Mathf.Rad2Deg;
-    }
 
     /// <summary>
     ///   Returns the nearest float to value that is a multiplier of factor.
@@ -36,8 +28,8 @@ namespace Utilities {
     ///   Returns the distance between two values in modulo space.
     /// </summary>
     public static float DistanceInModulo(float value, float target, float modulo) {
-      var diff = Mathf.Abs(value - target);
-      return Mathf.Min(diff, modulo - diff);
+      var diff = Math.Abs(value - target);
+      return Math.Min(diff, modulo - diff);
     }
   }
 }
--- Unity/Math.cs
+++ Unity/Math.cs
@@ -9,5 +9,13 @@ namespace Utilities {
     public static float AngleSigned(Vector3 v1, Vector3 v2, Vector3 n) {
       return Mathf.Atan2(Vector3.Dot(n, Vector3.Cross(v1, v2)), Vector3.Dot(v1, v2)) * Mathf.Rad2Deg;
     }
+
+    /// <summary>
+    ///   Determine the signed angle between two vectors, with normal 'n'
+    ///   as the rotation axis.
+    /// </summary>
+    public static float AngleSigned(Vector3 v1, Vector3 v2, Vector3 n) {
+      return Mathf.Atan2(Vector3.Dot(n, Vector3.Cross(v1, v2)), Vector3.Dot(v1, v2)) * Mathf.Rad2Deg;
+    }
   }
 }
