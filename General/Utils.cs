be29548642340b37d28eab4a4ae1cc19dc356e00
John Austin <kleptine@gmail.com>
2017-07-05 15:40:23 -0700

    Reformat to 4 indents, braces on next line.

--- General/Utils.cs
+++ General/Utils.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using Optional;
 
-public static class Utils {
-  public static class EnumUtil {
-    public static IEnumerable<T> GetEnumValues<T>() {
-      return Enum.GetValues(typeof(T)).Cast<T>();
+public static class Utils
+{
+    public static class EnumUtil
+    {
+        public static IEnumerable<T> GetEnumValues<T>()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
     }
-  }
 
-  public static T UnwrapOrDefault<T, V>(this Option<V> option, Func<V, T> map, T default_value) {
-    return option.Match(map, () => default_value);
-  }
+    public static T UnwrapOrDefault<T, V>(this Option<V> option, Func<V, T> map, T default_value)
+    {
+        return option.Match(map, () => default_value);
+    }
 }
