73a032a317dc9188b0322c607d434675a6d55bf3
John Austin <kleptine@gmail.com>
2017-09-29 12:52:43 -0700

    Fix formatting and warnings.

--- Unity/PrintLogToScreen.cs
+++ Unity/PrintLogToScreen.cs
@@ -1,36 +1,49 @@
-﻿using System.Collections;
+﻿using System;
 using UnityEngine;
 
-public class PrintLogToScreen : MonoBehaviour {
-  private readonly Queue my_log_queue = new Queue();
-  private string log;
+namespace Utilities.Unity
+{
+    public class PrintLogToScreen : MonoBehaviour
+    {
+        private string log;
 
-  private void Start() {
-    Debug.Log("Log now printing to screen...");
-  }
+        private void OnEnable()
+        {
+            Application.logMessageReceived += HandleLog;
+        }
 
-  private void Update() {
-  }
+        private void OnDisable()
+        {
+            Application.logMessageReceived -= HandleLog;
+        }
 
-  private void OnEnable() {
-    Application.logMessageReceived += HandleLog;
-  }
+        private void HandleLog(string logString, string stackTrace, LogType type)
+        {
+            string line = "[" + type + "] : " + log + "\n";
+            log += line;
 
-  private void OnDisable() {
-    Application.logMessageReceived -= HandleLog;
-  }
+            switch (type)
+            {
+                case LogType.Exception:
+                    line = stackTrace + "\n";
+                    log += line;
+                    break;
+                case LogType.Error:
+                    break;
+                case LogType.Assert:
+                    break;
+                case LogType.Warning:
+                    break;
+                case LogType.Log:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("type", type, null);
+            }
+        }
 
-  private void HandleLog(string log_string, string stack_trace, LogType type) {
-    string line = "[" + type + "] : " + log + "\n";
-    log += line;
-
-    if (type == LogType.Exception) {
-      line = stack_trace + "\n";
-      log += line;
+        private void OnGUI()
+        {
+            GUILayout.Label(log);
+        }
     }
-  }
-
-  private void OnGUI() {
-    GUILayout.Label(log);
-  }
 }
