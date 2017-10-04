10f312648c81b03c5c3ce1dcf609c325760b5c8a
John Austin <kleptine@gmail.com>
2017-10-04 10:35:22 -0700

    Adds content fitter component for sizing images and other objects.

---
+++ Unity/ContentAspectRatioFitter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions.Comparers;
+using UnityEngine.UI;
+
+namespace Utilities.Unity
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(RectTransform))]
+    [DisallowMultipleComponent]
+    public class ContentAspectRatioFitter : AspectRatioFitter
+    {
+        protected override void OnRectTransformDimensionsChange()
+        {
+            var width = LayoutUtility.GetPreferredSize(GetComponent<RectTransform>(), 0);
+            var height = LayoutUtility.GetPreferredSize(GetComponent<RectTransform>(), 1);
+
+            var ratio = width / height;
+
+            if (!float.IsNaN(ratio))
+            {
+                aspectRatio = Mathf.Clamp(ratio, 1f / 1000f, 1000f);
+            }
+
+            base.OnRectTransformDimensionsChange();
+        }
+    }
+}
