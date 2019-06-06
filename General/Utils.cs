0ea7d09007c980d4707c4ab65e2df4fdf117a39e
John Austin <kleptine@gmail.com>
2019-06-05 19:16:58 -0700

    Adds comments and some dictionary utilities.

--- General/Utils.cs
+++ General/Utils.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
@@ -54,6 +53,7 @@ public static class Utils
             }
         }
     }
+
     public static class EnumUtil
     {
         public static IEnumerable<T> GetEnumValues<T>()
@@ -68,20 +68,63 @@ public static class Utils
     public static double TicksToMs(long ticks)
     {
         Debug.Assert(Stopwatch.IsHighResolution);
-        return (double)ticks / Stopwatch.Frequency * 1000.0;
+        return (double) ticks / Stopwatch.Frequency * 1000.0;
     }
 
+    /// <summary>
+    /// Unwraps the given option. If the option is a None, returns the given default value instead.
+    /// </summary>
     public static T UnwrapOrDefault<T, V>(this Option<V> option, Func<V, T> map, T defaultValue)
     {
         return option.Match(map, () => defaultValue);
     }
 
+
+    /// <summary>
+    /// A <see cref="Condition"/> validator to ensure that an integer is divisible by some modulo.
+    /// </summary>
     public static ConditionValidator<int> IsDivisibleBy(this ConditionValidator<int> validator, int modulo)
     {
         if (validator.Value % modulo != 0)
         {
-            throw new AssertionException(validator.ArgumentName + ": " + validator.Value + " is not divisible by " + modulo, "");
+            throw new AssertionException(
+                $"{validator.ArgumentName}: {validator.Value} is not divisible by {modulo}", "");
         }
+
         return validator;
+    }
+
+    /// <summary>
+    /// Looks for a key in the dictionary, and returns its value. If it is not found, returns the provided default value.
+    /// </summary>
+    public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key,
+                                                         TValue defaultValue)
+    {
+        return dictionary.TryGetValue(key, out TValue value) ? value : defaultValue;
+    }
+
+    /// <summary>
+    /// Looks for a key in the dictionary, and returns its value. If it is not found, returns the provided default value, using the provider function.
+    /// </summary>
+    public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key,
+                                                         Func<TValue> defaultValueProvider)
+    {
+        return dictionary.TryGetValue(key, out TValue value) ? value : defaultValueProvider();
+    }
+
+    /// <summary>
+    /// Gets the value associated with the given key. If not found, inserts the provided value, using the provider function, then returns the value.
+    /// </summary>
+    public static TValue GetOrInsert<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key,
+                                                   Func<TValue> valueToInsert)
+    {
+        if (dictionary.ContainsKey(key))
+        {
+            return dictionary[key];
+        }
+
+        var value = valueToInsert();
+        dictionary[key] = value;
+        return value;
     }
 }
