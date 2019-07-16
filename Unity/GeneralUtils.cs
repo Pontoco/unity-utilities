781b1c205a2e2ae2bc20d15ab47ae5f5320713cc
John Austin <kleptine@gmail.com>
2019-07-15 19:52:47 -0700

    Add a simple method to get a child by name recursively.

--- Unity/GeneralUtils.cs
+++ Unity/GeneralUtils.cs
@@ -25,5 +25,29 @@ namespace Utilities.Unity
                 yield return root.GetChild(i);
             }
         }
+
+        /// <summary>
+        /// Returns the first child with the given name.
+        /// </summary>
+        public static Transform FirstChildOrDefault(this Transform parent, string name)
+        {
+            if (parent.childCount == 0)
+            {
+                return null;
+            }
+
+            Transform result = null;
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                var child = parent.GetChild(i);
+                if (child.name == name)
+                {
+                    return child;
+                }
+                result = FirstChildOrDefault(child, name);
+            }
+
+            return result;
+        }
     }
 }
