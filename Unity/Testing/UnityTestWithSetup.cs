b7269a5b85246ae649b685d9b8ac86db853f1a8a
John Austin <kleptine@gmail.com>
2019-07-17 08:26:44 -0700

    Better commenting.

--- Unity/Testing/UnityTestWithSetup.cs
+++ Unity/Testing/UnityTestWithSetup.cs
@@ -8,6 +8,13 @@ namespace ASG.Utilities.Unity.Testing
         /// <inheritdoc />
         public virtual void Setup()
         {
+            // Test Setup runs after the test scene is loaded, but before hitting 'play'
+        }
+
+        /// <summary>
+        /// Adds the Test Harness prefab that contains the game harness and some testing scripts.
+        /// </summary>
+        public void SetupTestHarness() {
             // Add our canary that sits around during tests.
             Debug.Log("Setup test harness to catch logged errors.");
 
