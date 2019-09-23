55b77bf49030da25711566114f93b7a02f9c2950
John Austin <kleptine@gmail.com>
2019-09-23 11:45:11 -0700

    Selection base as a component.

---
+++ Unity/SelectionBaseObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Core.Plugins.AsgUtils.Unity
+{
+    /// <summary>
+    ///     Add this to a game object to make it select by default when any of the children are selected via the mouse in
+    ///     the editor scene view.
+    /// </summary>
+    [SelectionBase]
+    public class SelectionBaseObject : MonoBehaviour
+    {
+    }
+}
