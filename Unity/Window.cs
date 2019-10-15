9842c9bb27b3a33bc50ed6b4435500850ef133dc
John Austin <kleptine@gmail.com>
2019-10-15 11:53:39 -0700

    Adds the Window class for tracking values over time.

---
+++ Unity/Window.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+using Utilities;
+
+public class Window<T>
+{
+    private T[] array;
+    private int nextIndex = 0;
+
+    public int CurrentSize { get; private set; }
+
+    public Window(int size)
+    {
+        array = new T[size];
+    }
+
+    public void Add(T value)
+    {
+        array[nextIndex] = value;
+        nextIndex++;
+        nextIndex = NumUtils.Mod(nextIndex, array.Length);
+
+        if (CurrentSize < array.Length)
+        {
+            CurrentSize++;
+        }
+    }
+
+    public IEnumerable<T> GetValues()
+    {
+        int startIndex;
+        if (CurrentSize == array.Length)
+        {
+            startIndex = nextIndex;
+            startIndex = NumUtils.Mod(startIndex, array.Length);
+        }
+        else
+        {
+            startIndex = 0;
+        }
+
+        for (int i = 0; i < CurrentSize; i++)
+        {
+            yield return array[startIndex];
+            startIndex++;
+            startIndex = NumUtils.Mod(startIndex, array.Length);
+        }
+
+        yield break;
+    }
+}
