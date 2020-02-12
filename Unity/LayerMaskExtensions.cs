0ea2ff8785f47abf9b132cc96d0eb827340c0e36
Matt Blair <blair1618@gmail.com>
2020-02-12 02:39:15 -0500

    Add extension method to check layers in LayerMask

---
+++ Unity/LayerMaskExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace AsgUtils.Unity
+{
+    public static class LayerMaskExtensions
+    {
+        /// <summary>Returns true if the bit for the given layer is set in this mask, otherwise returns false.</summary>
+        public static bool Contains(this LayerMask mask, int layer)
+        {
+            return ((1 << layer) & mask) != 0;
+        }
+    }
+}
